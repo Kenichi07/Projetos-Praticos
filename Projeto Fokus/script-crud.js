0c823ae6a962c1534f0a046211214e7a78a2a6e4
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-06 22:19:10 -0300

    feat: created atualizarTarefa and event for btnCancelar

--- Projeto Fokus/script-crud.js
+++ Projeto Fokus/script-crud.js
@@ -2,8 +2,13 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+
+function atualizarTarefa() {
+  localStorage.setItem("tarefas", JSON.stringify(tarefas));
+}
 
 function criarElementoTarefa(tarefa) {
   const li = document.createElement("li");
@@ -18,10 +23,20 @@ function criarElementoTarefa(tarefa) {
 
   const paragrafo = document.createElement("p");
   paragrafo.textContent = tarefa.descricao;
-  paragrafo.classList.add('app__section-task-list-item-description')
+  paragrafo.classList.add("app__section-task-list-item-description");
 
   const botao = document.createElement("button");
-  botao.classList.add('app_button-edit')
+  botao.classList.add("app_button-edit");
+
+  botao.onclick = () => {
+    const novaDescricao = prompt("Qual é o novo nome da tarefa?");
+    console.log('Nova descrição da tarefa: ', novaDescricao)
+    if (novaDescricao) {
+      paragrafo.textContent = novaDescricao;
+      tarefa.descricao = novaDescricao;
+      atualizarTarefa();
+    }
+  };
 
   const imagemBotao = document.createElement("img");
   imagemBotao.setAttribute("src", "./imagens/edit.png");
@@ -44,14 +59,21 @@ formAdicionarTarefa.addEventListener("submit", (evento) => {
     descricao: textarea.value,
   };
   tarefas.push(tarefa);
-  const elementoTarefa = criarElementoTarefa(tarefa)
-  ulTarefas.append(elementoTarefa)
-  localStorage.setItem("tarefas", JSON.stringify(tarefas));
-  textarea.value = ''
-  formAdicionarTarefa.classList.add('hidden')
+  const elementoTarefa = criarElementoTarefa(tarefa);
+  ulTarefas.append(elementoTarefa);
+  atualizarTarefa();
+  textarea.value = "";
+  formAdicionarTarefa.classList.add("hidden");
 });
 
 tarefas.forEach((tarefa) => {
   const elementoTarefa = criarElementoTarefa(tarefa);
   ulTarefas.append(elementoTarefa);
 });
+
+const limparFormulario = () => {
+  textarea.value = '';  // Limpe o conteúdo do textarea
+  formularioTarefa.classList.add('hidden');  // Adicione a classe 'hidden' ao formulário para escondê-lo
+}
+
+btnCancelar.addEventListener('click', limparFormulario);
