4b96dc2ab62b5df36b51e7393e70e928019b66c7
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-07 12:39:12 -0300

    feat: created change task to complete

--- Projeto Fokus/script-crud.js
+++ Projeto Fokus/script-crud.js
@@ -2,9 +2,15 @@ const btnAdicionarTarefa = document.querySelector(".app__button--add-task");
 const formAdicionarTarefa = document.querySelector(".app__form-add-task");
 const textarea = document.querySelector(".app__form-textarea");
 const ulTarefas = document.querySelector(".app__section-task-list");
-const btnCancelar = document.querySelector('.app__form-footer__button--cancel');
+const paragrafoDescricaoTarefa = document.querySelector(
+  ".app__section-active-task-description"
+);
+
+const btnCancelar = document.querySelector(".app__form-footer__button--cancel");
 
 const tarefas = JSON.parse(localStorage.getItem("tarefas")) || [];
+let tarefaSelecionada = null;
+let liTarefaSelecionada = null;
 
 function atualizarTarefa() {
   localStorage.setItem("tarefas", JSON.stringify(tarefas));
@@ -29,8 +35,9 @@ function criarElementoTarefa(tarefa) {
   botao.classList.add("app_button-edit");
 
   botao.onclick = () => {
+    //debugger
     const novaDescricao = prompt("Qual é o novo nome da tarefa?");
-    console.log('Nova descrição da tarefa: ', novaDescricao)
+    //console.log('Nova descrição da tarefa: ', novaDescricao)
     if (novaDescricao) {
       paragrafo.textContent = novaDescricao;
       tarefa.descricao = novaDescricao;
@@ -45,6 +52,30 @@ function criarElementoTarefa(tarefa) {
   li.append(svg);
   li.append(paragrafo);
   li.append(botao);
+
+  if (tarefa.completa) {
+    li.classList.add("app__section-task-list-item-complete");
+    botao.setAttribute("disabled", "disabled");
+  } else {
+    li.onclick = () => {
+      document
+        .querySelectorAll(".app__section-task-list-item-active")
+        .forEach((elemento) => {
+          elemento.classList.remove("app__section-task-list-item-active");
+        });
+      if (tarefaSelecionada == tarefa) {
+        paragrafoDescricaoTarefa.textContent = "";
+        tarefaSelecionada = null;
+        liTarefaSelecionada = null;
+        return;
+      }
+      tarefaSelecionada = tarefa;
+      liTarefaSelecionada = li;
+      paragrafoDescricaoTarefa.textContent = tarefa.descricao;
+
+      li.classList.add("app__section-task-list-item-active");
+    };
+  }
 
   return li;
 }
@@ -72,8 +103,20 @@ tarefas.forEach((tarefa) => {
 });
 
 const limparFormulario = () => {
-  textarea.value = '';  // Limpe o conteúdo do textarea
-  formularioTarefa.classList.add('hidden');  // Adicione a classe 'hidden' ao formulário para escondê-lo
-}
+  textarea.value = ""; // Limpe o conteúdo do textarea
+  formularioTarefa.classList.add("hidden"); // Adicione a classe 'hidden' ao formulário para escondê-lo
+};
 
-btnCancelar.addEventListener('click', limparFormulario);
+btnCancelar.addEventListener("click", limparFormulario);
+
+document.addEventListener("FocoFinalizado", () => {
+  if (tarefaSelecionada && liTarefaSelecionada) {
+    liTarefaSelecionada.classList.remove("app__section-task-list-item-active");
+    liTarefaSelecionada.classList.add("app__section-task-list-item-complete");
+    liTarefaSelecionada
+      .querySelector("button")
+      .setAttribute("disabled", "disabled");
+    tarefaSelecionada.completa = true;
+    atualizarTarefa();
+  }
+});
--- Projeto Fokus/script.js
+++ Projeto Fokus/script.js
@@ -29,7 +29,7 @@ musicaFocoInput.addEventListener("change", () => {
 });
 
 focoBt.addEventListener("click", () => {
-  tempoDecorridoEmSegundos = 1500;
+  tempoDecorridoEmSegundos = 10;
   alterarContexto("foco");
   focoBt.classList.add("active");
 });
@@ -75,6 +75,11 @@ const contagemRegressiva = () => {
   if (tempoDecorridoEmSegundos <= 0) {
     audioTempoFinalizado.play();
     alert("Tempo Finalizado!");
+    const focaAtivo = html.getAttribute('data-contexto') == 'foco'
+    if (focaAtivo) {
+      const evento = new CustomEvent('FocoFinalizado')
+      document.dispatchEvent(evento)
+    }
     zerar();
     return;
   }
