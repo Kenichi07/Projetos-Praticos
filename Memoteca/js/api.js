9daf112dfc9cc7586efcfae1b65d5d4d4ff0cda1
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-09 11:38:52 -0300

    feat: created saveMemo

--- Memoteca/js/api.js
+++ Memoteca/js/api.js
@@ -8,6 +8,21 @@ const api = {
       throw error;
     }
   },
+  async salvarPensamentos(pensamento) {
+    try {
+      const response = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return response.json();
+    } catch (error) {
+      alert("Erro ao salvar pensamentos");
+      throw error;
+    }
+  },
 };
 
 export default api;
