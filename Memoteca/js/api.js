30eddc0752150f53930412cd9736c81ea16f3c0f
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-10 10:27:40 -0300

    feat: created buscarPensamentoPorTermo

--- Memoteca/js/api.js
+++ Memoteca/js/api.js
@@ -52,6 +52,23 @@ const api = {
       throw error;
     }
   },
+  async buscarPensamentoPorTermo(termo) {
+    try {
+      const pensamentos = await this.buscarPensamentos();
+      const termoEmMinusculas = termo.toLowerCase();
+
+      const pensamentosFiltrados = pensamentos.filter((pensamento) => {
+        return (
+          pensamento.conteudo.toLowerCase().includes(termoEmMinusculas) ||
+          pensamento.autoria.toLowerCase().includes(termoEmMinusculas)
+        );
+      });
+      return pensamentosFiltrados;
+    } catch (error) {
+      alert("Erro ao filtrar pensamentos");
+      throw error;
+    }
+  },
 };
 
 export default api;
