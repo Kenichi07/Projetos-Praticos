2359f2c258adc95aab949bb2d3269eb26deb778f
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-09 23:27:46 -0300

    feat: created excluirPensamento

--- Memoteca/js/api.js
+++ Memoteca/js/api.js
@@ -8,7 +8,7 @@ const api = {
       throw error;
     }
   },
-  async salvarPensamentos(pensamento) {
+  async salvarPensamento(pensamento) {
     try {
       const response = await fetch("http://localhost:3000/pensamentos", {
         method: "POST",
@@ -32,18 +32,31 @@ const api = {
       throw error;
     }
   },
-  async editarPensamentos(pensamento) {
+  async editarPensamento(pensamento) {
     try {
-      const response = await fetch(`http://localhost:3000/pensamentos/${pensamento.id}`, {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(pensamento),
-      });
+      const response = await fetch(
+        `http://localhost:3000/pensamentos/${pensamento.id}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(pensamento),
+        }
+      );
       return await response.json();
     } catch (error) {
       alert("Erro ao editar pensamento");
+      throw error;
+    }
+  },
+  async excluirPensamento(id) {
+    try {
+      const response = await fetch(`http://localhost:3000/pensamentos/${id}`, {
+        method: "DELETE",
+      });
+    } catch (error) {
+      alert("Erro ao excluir um pensamento");
       throw error;
     }
   },
