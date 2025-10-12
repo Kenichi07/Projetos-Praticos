5cd0bd849d205807bd4830411b1f00ffdc486cfa
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-12 15:45:50 -0300

    feat: created validation about pensamento exist or not

--- Memoteca/js/main.js
+++ Memoteca/js/main.js
@@ -1,19 +1,38 @@
 import ui from "./ui.js";
 import api from "./api.js";
 
-const regexConteudo = /^[A-Za-z\s]{10,}$/
-const regexAutoria = /^[A-Za-z]{3,15}$/
+const pensamentosSet = new Set();
+const regexConteudo = /^[A-Za-z\s]{10,}$/;
+const regexAutoria = /^[A-Za-z]{3,15}$/;
 
-function validarConteudo (conteudo) {
-  return regexConteudo.test(conteudo)
+async function adicionarChaveAoPensamento() {
+  try {
+    const pensamentos = await api.buscarPensamentos();
+    pensamentos.forEach((pensamento) => {
+      const chavePensamento = `${
+        pensamento.conteudo.trim().toLowerCase
+      }-${pensamento.autoria.trim().toLowerCase()}`;
+      pensamentosSet.add(chavePensamento);
+    });
+  } catch (error) {
+    alert("Erro ao adicionar chave ao pensamento.");
+  }
+}
+function validarConteudo(conteudo) {
+  return regexConteudo.test(conteudo);
 }
 
-function validarAutoria (autoria) {
-  return regexAutoria.test(autoria)
+function validarAutoria(autoria) {
+  return regexAutoria.test(autoria);
+}
+
+function removerEspacos(string) {
+  return string.replaceAll(/\s+/g, "");
 }
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos();
+  adicionarChaveAoPensamento();
 
   const formularioPensamento = document.getElementById("pensamento-form");
   const botaoCancelar = document.getElementById("botao-cancelar");
@@ -31,18 +50,34 @@ async function manipularSubmissaoFormulario(event) {
   const autoria = document.getElementById("pensamento-autoria").value;
   const data = document.getElementById("pensamento-data").value;
 
-  if (!validarConteudo(conteudo)) {
-    alert("É permitida a inclusão apenas de letras e espaços com no mínimo 10 caracteres")
-    return
+  const conteudoSemEspacos = removerEspacos(conteudo);
+  const autoriaSemEspacos = removerEspacos(autoria);
+
+  if (!validarConteudo(conteudoSemEspacos)) {
+    alert(
+      "É permitida a inclusão apenas de letras e espaços com no mínimo 10 caracteres"
+    );
+    return;
   }
 
-  if (!validarAutoria(autoria)) {
-    alert("É permitida a inclusão de letras e entre 3 e 15 caracteres sem espaços")
-    return
+  if (!validarAutoria(autoriaSemEspacos)) {
+    alert(
+      "É permitida a inclusão de letras e entre 3 e 15 caracteres sem espaços"
+    );
+    return;
   }
 
   if (!validarData(data)) {
-    alert("Não é permitido o cadastro de datas futuras! Selecione outra data.")
+    alert("Não é permitido o cadastro de datas futuras! Selecione outra data.");
+  }
+
+  const chaveNovoPensamento = `${conteudo.trim().toLowerCase}-${autoria
+    .trim()
+    .toLowerCase()}`;
+
+  if (pensamentosSet.has(chaveNovoPensamento)) {
+    alert("Esse pensamento já existe.");
+    return
   }
 
   try {
@@ -67,12 +102,12 @@ async function manipularBusca() {
     const pensamentosFiltrados = await api.buscarPensamentoPorTermo(termoBusca);
     ui.renderizarPensamentos(pensamentosFiltrados);
   } catch (error) {
-    alert("Erro ao realizar busca")
+    alert("Erro ao realizar busca");
   }
 }
 
-function validarData (data) {
-  const dataAtual = new Date()
-  const dataInserida = new Date(data)
-  return dataInserida <= dataAtual
+function validarData(data) {
+  const dataAtual = new Date();
+  const dataInserida = new Date(data);
+  return dataInserida <= dataAtual;
 }
