a7b2fd9a8c8bb94b11bbdb58f38ee10a70adf8e9
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-17 11:47:05 -0300

    feat: continue focus when close modal

--- Calmaria-Spa/main.js
+++ Calmaria-Spa/main.js
@@ -1,3 +1,4 @@
+let ultimoElementoFocado;
 function gerenciarFocoModal(modalId) {
   const modal = document.querySelector(`#${modalId}`);
   const elementosModal = modal.querySelectorAll(
@@ -8,16 +9,39 @@ function gerenciarFocoModal(modalId) {
   const ultimoElemento = elementosModal[elementosModal.length - 1];
 
   primeiroElemento.focus();
+
+  modal.addEventListener("keydown", (event) => {
+    if (event.key === "Tab") {
+      if (event.shiftKey) {
+        if (document.activeElement === primeiroElemento) {
+          event.preventDefault();
+          ultimoElemento.focus();
+        }
+      } else {
+        if (
+          document.activeElement === ultimoElemento ||
+          !modal.contains(document.activeElement)
+        ) {
+          event.preventDefault();
+          primeiroElemento.focus();
+        }
+      }
+    }
+  });
 }
 
 function alternarModal(modalId, abrir) {
   const modal = document.querySelector(`#${modalId}`);
 
   if (abrir) {
+    ultimoElementoFocado = document.activeElement;
     modal.style.display = "block";
     gerenciarFocoModal(modalId);
   } else {
     modal.style.display = "none";
+    if (ultimoElementoFocado) {
+      ultimoElementoFocado.focus();
+    }
   }
 
   document.body.style.overflow = abrir ? "hidden" : "auto";
