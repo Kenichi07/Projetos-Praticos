9e636ed4b7d81ed8159d678fe8353ce725fa448f
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-21 20:38:58 -0300

    feat: created editarItens

--- Lista-de-compras-2/main.js
+++ Lista-de-compras-2/main.js
@@ -1,4 +1,5 @@
 let listaDeItens = [];
+let itemAEditar;
 
 const form = document.getElementById("form-itens");
 const itensInput = document.getElementById("receber-item");
@@ -53,6 +54,8 @@ function mostrarItem() {
             <input type="text" class="is-size-5" value="${elemento.valor}"></input>
         </div>
         <div>
+        <button onClick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>
+        <i class="fa-regular is-clickable fa-pen-to-square editar"></i>
             <i class="fa-solid fa-trash is-clickable deletar"></i>
         </div>
     </li>
@@ -81,4 +84,25 @@ function mostrarItem() {
       mostrarItem();
     });
   });
+
+  const editarItens = document.querySelectorAll(".editar");
+
+  editarItens.forEach((i) => {
+    i.addEventListener("click", (evento) => {
+      itemAEditar =
+        evento.target.parentElement.parentElement.getAttribute("data-value");
+      mostrarItem();
+      console.log(itemAEditar);
+    });
+  });
+}
+
+function salvarEdicao() {
+  const itemEditado = document.querySelector(
+    `[data-value="${itemAEditar}"] input[type="text"]`
+  );
+  listaDeItens[itemAEditar].valor = itemEditado.value;
+  console.log(listaDeItens);
+  itemAEditar = -1;
+  mostrarItem();
 }
