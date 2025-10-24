b35b169ae96b7f6424498685749a1bf576021296
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-24 12:07:47 -0300

    feat: created render in personagem-view

--- Devs&Dragons/src/index.js
+++ Devs&Dragons/src/index.js
@@ -1,5 +1,10 @@
 import { Personagem } from "./modules/personagem.js";
+import { personagemView } from "./components/personagem-view.js";
 
 const personagemPedrinho = new Personagem("Pedrinho", 5, "Mago");
 
 const personagemJose = new Personagem("Jose", 3, "Arqueiro");
+
+const personagens = [personagemPedrinho, personagemJose];
+
+new personagemView(personagens).render();
