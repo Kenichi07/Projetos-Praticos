ed4d43808bb2505be7e25c22af50e851240823f8
Vinicius Huang <viniciushuangbr@gmail.com>
2025-10-27 11:22:11 -0300

    feat: created geraHorario and adicionarDados

---
+++ Bytebank/script/script.js
@@ -0,0 +1,42 @@
+const graficoDolar = document.getElementById("graficoDolar");
+
+const graficoParaDolar = new Chart(graficoDolar, {
+  type: "line",
+  data: {
+    labels: [],
+    datasets: [
+      {
+        label: "Dolar",
+        data: [],
+        borderWidth: 1,
+      },
+    ],
+  },
+});
+
+async function conectaAPI() {
+  const conecta = await fetch(
+    "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+  );
+  const conectaTraduzido = await conecta.json();
+  let tempo = geraHorario();
+  let valor = conectaTraduzido.USDBRL.ask;
+  adicionarDados(graficoParaDolar, tempo, valor);
+}
+
+setInterval(() => conectaAPI(), 5000);
+
+function geraHorario() {
+  let data = new Date();
+  let horario =
+    data.getHours() + ":" + data.getMinutes() + ":" + data.getSeconds();
+  return horario;
+}
+
+function adicionarDados(grafico, legenda, dados) {
+  grafico.data.labels.push(legenda);
+  grafico.data.datasets.forEach((dataset) => {
+    dataset.data.push(dados);
+  });
+  grafico.update();
+}
